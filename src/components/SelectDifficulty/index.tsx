1ec33a2449d4b0d5b1bf893404793c0a504807a9
Andre Grossi <iamandregrossi@gmail.com>
2020-09-04 23:19:54 -0300

    Extract styles from Card to SelectDifficulty

--- src/components/SelectDifficulty/index.tsx
+++ src/components/SelectDifficulty/index.tsx
@@ -7,23 +7,27 @@ import { useQuiz } from '../../hooks/useQuiz'
 import { difficulties } from '../../parameters/difficulties'
 import { Difficulty } from '../../models'
 
+import { Contents } from './styles'
+
 const SelectDifficulty: React.FC = () => {
   const { chooseDifficulty } = useQuiz()
 
   return (
-    <Card>
-      <h2>Choose a difficulty:</h2>
-      {difficulties.map((difficulty: Difficulty) => (
-        <Choice
-          key={difficulty.name}
-          clickAction={() => chooseDifficulty(difficulty)}
-          choiceType="difficulty"
-        >
-          <strong>{difficulty.name}</strong>
-          <span>{difficulty.numQuestions} questions </span>
-          <span>{difficulty.numCountries} countries </span>
-        </Choice>
-      ))}
+    <Card hasImage>
+      <Contents>
+        <h2>Choose a difficulty:</h2>
+        {difficulties.map((difficulty: Difficulty) => (
+          <Choice
+            key={difficulty.name}
+            clickAction={() => chooseDifficulty(difficulty)}
+            choiceType="difficulty"
+          >
+            <strong>{difficulty.name}</strong>
+            <span>{difficulty.numQuestions} questions </span>
+            <span>{difficulty.numCountries} countries </span>
+          </Choice>
+        ))}
+      </Contents>
     </Card>
   )
 }
---
+++ src/components/SelectDifficulty/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Contents = styled.div`
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  h2 {
+    font-size: 1.4em;
+    color: var(--color-question-title);
+    margin-bottom: 1.6em;
+  }
+`
