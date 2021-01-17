88ba3588a2348fbe27803754351098cf03b772c3
Andre Grossi <iamandregrossi@gmail.com>
2021-01-17 18:06:19 -0300

    Discard retrieving of region info from API

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { Country } from '../models'
 
 export const getCountries = async (countries: string): Promise<Country[]> => {
   const res = await fetch(
-    `https://restcountries.eu/rest/v2/alpha?codes=${countries}&fields=name;capital;region;flag`
+    `https://restcountries.eu/rest/v2/alpha?codes=${countries}&fields=name;capital;flag`
   )
   return res.json()
 }
