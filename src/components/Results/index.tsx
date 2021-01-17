d7c212b3930f1f3a24fed80e03b2db4bd8564b6a
Andre Grossi <iamandregrossi@gmail.com>
2021-01-17 20:37:59 -0300

    Fix results count display

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -9,18 +9,26 @@ import { useQuiz } from '../../hooks/useQuiz'
 const Results: React.FC = () => {
   const { score, restartQuiz } = useQuiz()
 
+  const scoreText = () => {
+    switch (score) {
+      case 0:
+        return 'Aww you got no correct answers :('
+      default:
+        return (
+          <>
+            You got <strong>{score}</strong> answer{score > 1 ? 's' : ''}{' '}
+            correct.
+          </>
+        )
+    }
+  }
+
   return (
     <Card>
       <Contents>
         <img src={resultsImg} alt="Quiz results" />
         <h1>Results</h1>
-        <span>
-          {score
-            ? `You got ${(<strong>{score}</strong>)} correct answer${
-                score > 1 ? 's' : ''
-              }`
-            : 'Aww you got no correct answers :('}
-        </span>
+        <span>{scoreText()}</span>
 
         <Button variant="outline" onClick={restartQuiz}>
           Try again
